e9f603bd508d2f7f1b6c0c549e7b4fdc6f8cb94a
Edgarmad <54693845+Edgarmad@users.noreply.github.com>
2025-03-18 20:44:03 -0600

    cambios en logica de flores para agregar mas flores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,21 +13,42 @@ export class AppComponent {
   showPopup = false;
   dibujarFlores() {
     const lluviaFlores = document.getElementById('lluviaFlores');
+    const imagenesFlores = ['flower_0.png', 'flower_1.png', 'flower_2.png', 'flower_3.png']; // Lista de imágenes
+  
     if (lluviaFlores) {
-      for (let i = 0; i < 1; i++) { // Esto puede cambiar, el número de flores que llueven
-        const flower = document.createElement('img');
-        flower.src = '/flores-placeholder.png'; // Ruta relativa del archivo PNG
-        flower.classList.add('flor'); // Añadir una clase para aplicar estilos
-        flower.style.position = 'absolute';
-        flower.style.left = `${Math.random() * 100}vw`; // Posición aleatoria en el ancho de la pantalla
-        flower.style.animation = `caer ${Math.random() * 2 + 3}s linear infinite`; // Tiempo de caída aleatorio
-        lluviaFlores.appendChild(flower);
+      const cantidadFlores = 30; // Número total de flores
+      const intervalo = 200; // Tiempo en ms entre cada flor
+  
+      for (let i = 0; i < cantidadFlores; i++) {
+        setTimeout(() => {
+          const flower = document.createElement('img');
+          const imagenAleatoria = imagenesFlores[Math.floor(Math.random() * imagenesFlores.length)];
+          flower.src = `/${imagenAleatoria}`;
+          flower.style.position = 'fixed';
+          flower.style.left = `${Math.random() * 100}vw`;
+          flower.style.top = `${-10 - Math.random() * 20}vh`;
+          const size = `${Math.random() * 1 + 5}rem`; 
+          flower.style.width = size;
+          flower.style.height = size;
+  
+          // Animacion de caída
+          flower.style.animation = `caer ${Math.random() * 3 + 2}s linear forwards`;
+          lluviaFlores.appendChild(flower);
+  
+          setTimeout(() => {
+            flower.remove();
+          }, 5000);
+        }, i * intervalo);
       }
     }
+  
+    // Mostrar el popup después de 5 segundos
     setTimeout(() => {
       this.showPopup = true;
     }, 5000);
   }
+  
+  
   cerrarPopup() {
     this.showPopup = false;
   }
